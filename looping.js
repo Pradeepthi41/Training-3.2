0150d4285d70833b91bb361cef49dbb33a6006c2
Pradeepthi41 <vpradeepthi41@gmail.com>
2025-01-08 10:27:39 +0530

    looping

---
+++ looping.js
@@ -0,0 +1,15 @@
+//let n=100;
+//for (let i = 0; i <= 100; i++) {
+  //  if (i % 2 === 0) {
+    //    console.log((i)+" is Even Number");
+    //}
+     //else {
+       // console.log((i)+" is Odd Number");
+    //}
+//}
+// let i=1;
+// while(i<=5){
+//     console.log("gm");
+//     i++;
+// }
+
