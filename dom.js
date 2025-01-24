0f735a9e073632bdcf01cb015db52d32af3edd26
Pradeepthi41 <vpradeepthi41@gmail.com>
2025-01-24 12:58:17 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,33 @@
+
+// console.log(window);
+// console.log(document);
+
+// let element = document.getElementById("demo");
+// element.innerText = "G+"
+// console.log(element);
+
+// let ele = document.getElementById("test");
+// // ele.innerText= "<h1>Header</h1>"
+// ele.innerHTML = "<h1>Header</h1>"
+// console.log(ele);
+ 
+// let ele=document.getElementsByClassName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+// //ele[0].style.backgroundColor="tomato"
+// //! spread operator:- it will takeout each and every value from the original array 
+// //! and store it inside one more new array(pure array).
+
+
+// let x=[...ele];
+// //console.log(x,Array.isArray(x));
+// x.map(element=>{
+//     //console.log(element);
+//     element.style.backgroundColor="pink"
+
+// })
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+
