889f379cd6bb0a3716e52035326ab820d885010c
Pradeepthi41 <vpradeepthi41@gmail.com>
2025-01-25 12:56:00 +0530

    DOM

--- dom.js
+++ dom.js
@@ -1,4 +1,3 @@
-
 // console.log(window);
 // console.log(document);
 
@@ -10,24 +9,118 @@
 // // ele.innerText= "<h1>Header</h1>"
 // ele.innerHTML = "<h1>Header</h1>"
 // console.log(ele);
- 
-// let ele=document.getElementsByClassName("test");
+
+//! 
+// let ele = document.getElementsByClassName("test");
 // console.log(ele);
 // console.log(Array.isArray(ele));
 // ele[0].innerText="Hello"
-// //ele[0].style.backgroundColor="tomato"
-// //! spread operator:- it will takeout each and every value from the original array 
-// //! and store it inside one more new array(pure array).
+// // ele[0].style.backgroundColor="tomato"
 
-
-// let x=[...ele];
-// //console.log(x,Array.isArray(x));
+// //!spread operator :- it will takeout each and every value from the original array 
+// //! and store it inisde one more new array(pure array).
+// //? syntax :- [...variable];
+// let x = [...ele];
+// // console.log(x , Array.isArray(x));
 // x.map(element=>{
-//     //console.log(element);
-//     element.style.backgroundColor="pink"
-
+//     // console.log(element);
+//     element.style.backgroundColor="orange"
 // })
+
+//! 
+
+// let ele = document.getElementsByTagName("div");
+// console.log(ele);
+
+//! 
+
+// let ele = document.getElementsByName("formData");
+// // console.log(ele , Array.isArray(ele));
+// [...ele].map(inp=>{
+//     console.log(inp.value);
+// })
+
+//! 
+// let ele = document.querySelector("#mru");
+// console.log(ele);
+
+// let ele = document.querySelector(".test");
+// console.log(ele);
 
 // let ele = document.querySelectorAll(".test");
 // console.log(ele);
+
+
+
+// let btn = document.querySelector("#btn");
+//! addEventListner("event" ,()=>{})
+    // btn.addEventListener("click",()=>{
+    //     // console.log("button clicked");
+    //     // alert("hi")
+    //     document.body.style.backgroundColor="tomato"
+    // })
+
+    // let btn = document.querySelector("#btn");
+    // btn.addEventListener("dblclick",()=>{
+    //     console.log("double clicked");
+    // })
+
+
+    // let divBlock = document.getElementById("divBlock");
+    // divBlock.addEventListener("mouseover",()=>{
+    //     document.body.style.backgroundColor="yellow"
+    //     divBlock.style.backgroundColor="blue"
+    // })
+
+    // divBlock.addEventListener("mouseleave",()=>{
+    //     document.body.style.backgroundColor="orange"
+    //     divBlock.style.backgroundColor="aqua"
+    // })
+
+
+    // let inp =document.getElementById("inp");
+    // inp.addEventListener("keydown",()=>{
+    //     console.log("keydown");
+    // })
+
+    // inp.addEventListener("keyup",()=>{
+    //     console.log("keyup");
+    // })
+
+
+    let ele = document.createElement("h1");
+    ele.innerText = "DYN";
+    ele.setAttribute("id","demo");
+    ele.id ="demo";
+     
+    // ele.removedAttribute("id")
+    // console.log(ele);
+
+
+    let image = document.createElement("img");
+    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJOghGA66yFMDiApnvJOeGLRUSxZgdWYVIEQ&s";
+    console.log(image);
+
+    document.body.appendChild(ele);
+    document.body.appendChild(image);
+
+    let form = document.querySelector("form");
+    let username = document.getElementById("uName");
+    let mail = document.getElementById("uMail");
+    let psw = document.getElementById("uPass");
+
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        // console.log(event);
+        // console.log("form submitted");
+
+        let uname = username.value;
+        let uemail = mail.value;
+        let upass = psw.value;
+        let userDetails={
+            uname,uemail,upass
+        }
+        console.log(userDetails);
+        localStorage.setItem("userData",JSON.stringify(userDetails))
+        })
 
