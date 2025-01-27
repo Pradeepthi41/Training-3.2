38039b058310109da27a2b55e23bbc7abc52f3dd
Pradeepthi41 <vpradeepthi41@gmail.com>
2025-01-27 13:03:34 +0530

    hierarcy

--- dom.js
+++ dom.js
@@ -88,39 +88,94 @@
     // })
 
 
-    let ele = document.createElement("h1");
-    ele.innerText = "DYN";
-    ele.setAttribute("id","demo");
-    ele.id ="demo";
+     // let bgColor = document.querySelectorAll(".bgColor");
+    // // console.log(bgColor);
+    // [...bgColor].map((element)=>{
+    //     // console.log(element);
+    //     element.addEventListener("mouseover",()=>{
+    //         // console.log(element.innerText);
+    //         element.style.backgroundColor=element.innerText;
+    //     });
+    //     element.addEventListener("mouseleave",()=>{
+    //         // console.log(element.innerText);
+    //         element.style.backgroundColor="transparent";
+    //     });
+    // })
+
+
+
+    // let ele = document.createElement("h1");
+    // ele.innerText = "DYN";
+    // ele.setAttribute("id","demo");
+    // ele.id ="demo";
      
-    // ele.removedAttribute("id")
-    // console.log(ele);
+    // // ele.removedAttribute("id")
+    // // console.log(ele);
 
 
-    let image = document.createElement("img");
-    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJOghGA66yFMDiApnvJOeGLRUSxZgdWYVIEQ&s";
-    console.log(image);
+    // let image = document.createElement("img");
+    // image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJOghGA66yFMDiApnvJOeGLRUSxZgdWYVIEQ&s";
+    // console.log(image);
 
-    document.body.appendChild(ele);
-    document.body.appendChild(image);
+    // document.body.appendChild(ele);
+    // document.body.appendChild(image);
 
-    let form = document.querySelector("form");
-    let username = document.getElementById("uName");
-    let mail = document.getElementById("uMail");
-    let psw = document.getElementById("uPass");
+    // let form = document.querySelector("form");
+    // let username = document.getElementById("uName");
+    // let mail = document.getElementById("uMail");
+    // let psw = document.getElementById("uPass");
 
-    form.addEventListener("submit",(event)=>{
-        event.preventDefault();
-        // console.log(event);
-        // console.log("form submitted");
+    // form.addEventListener("submit",(event)=>{
+    //     event.preventDefault();
+    //     // console.log(event);
+    //     // console.log("form submitted");
 
-        let uname = username.value;
-        let uemail = mail.value;
-        let upass = psw.value;
-        let userDetails={
-            uname,uemail,upass
-        }
-        console.log(userDetails);
-        localStorage.setItem("userData",JSON.stringify(userDetails))
-        })
+    //     let uname = username.value;
+    //     let uemail = mail.value;
+    //     let upass = psw.value;
+    //     let userDetails={
+    //         uname,uemail,upass
+    //     }
+    //     console.log(userDetails);
+    //     localStorage.setItem("userData",JSON.stringify(userDetails))
+    //     })
+// localStorage.clear();
 
+let mainEle = document.createElement("div");
+mainEle.setAttribute("id","mainBlock");
+mainEle.style.border="2px solid red";
+mainEle.style.width="550px";
+mainEle.style.height="550px";
+console.log(mainEle);
+
+let topEle = document.createElement("div");
+topEle.setAttribute("class","topBlock");
+//console.log(topEle);
+
+let image = document.createElement("img");
+image.src="https://i.pinimg.com/originals/f0/eb/7e/f0eb7e99d19b68addc83a668de90489a.jpg";
+image.style.width="350px";
+
+let bottomEle = document.createElement("div");
+bottomEle.setAttribute("class","bottomBlock");
+//console.log(bottomEle);
+
+let h1 = document.createElement("h1");
+h1.innerText ="CAT";
+h1.style.textAlign="center"
+
+
+let btn = document.createElement("button");
+btn.innerText="view more";
+btn.style.border="none";
+btn.style.paddings="20px";
+btn.style.background="dodgerblue";
+btn.style.color="white";
+
+bottomEle.appendChild(h1);
+bottomEle.appendChild(btn);
+
+topEle.appendChild(image);
+mainEle.appendChild(topEle);
+mainEle.appendChild(bottomEle);
+document.body.appendChild(mainEle);
